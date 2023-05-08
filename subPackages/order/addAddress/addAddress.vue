e3ee7a3aaf3b6282ed9da9a4c9718d9330417470
DESKTOP-IBOGPBB\Dell <123456>
2023-05-08 11:11:30 +0800

    no message

--- subPackages/order/addAddress/addAddress.vue
+++ subPackages/order/addAddress/addAddress.vue
@@ -1,271 +1,336 @@
 <template>
-	<view class="addAddress paddTopheder">
-		<Header :title="this.uForm.id?'编辑地址':'添加地址'" :rightIcon="this.uForm.id?'删除':''" @rightClick="rightClick" />
-		<view class="addressBox">
-			<view class="tishi">
-				*根据监管要求，需要搜集您的详细地址用于货运运输
-			</view>
-			<view class="address">
-				<view class="left">
-					<view class="name">
-						{{uForm.name}}
-					</view>
-					<view class="xiangxi">
-						{{uForm.address}}
+	<view>
+		<Header title="添加地址" :PreventCollapse="true" :fontWeight="400" fontSize="32rpx" color="#000000"
+			:fanhui="true" />
+		<view class="box">
+			<map class="map" :scale="scale" :latitude="latitude" :longitude="longitude" :markers="marker">
+			</map>
+			<view class="routeAddressBox">
+				<view class="AddressInput">
+					<view class="input_wrapper">
+						<input placeholder="请填写地址" placeholder-style="fontSzie:24rpx;color:#A4A4A4;"
+							@input="changeShipInput" v-model="shippingValue" />
+						<image class="clearBtn" @click="clearShippingValue" v-show="shippingValue"
+							src="@/static/carType/close.png" mode="widthFix">
+						</image>
 					</view>
 				</view>
-				<view class="right" @click="changeOfAddress()">
-					更改地址
+				<view class="divider"></view>
+
+				<button class="submitBtn" @click="submitBtn">确定</button>
+
+			</view>
+			<!-- 消息提示 -->
+			<view class="messagePrompt">
+				<image class="iconPic" src="@/static/carType/sigh.png" mode="widthFix"></image>
+				<view class="text">
+					存在名称相似的不同地点，请仔细确认地址
 				</view>
 			</view>
-			<view class="jianjie">
-				<u--form labelPosition="left" :model="uForm" :rules="rules" ref="uForm"
-					:labelStyle="{'font-weight': 600,}" labelWidth="60">
-					<u-form-item label="门牌号" prop="detail">
-						<u--input v-model="uForm.detail" border="bottom" placeholder="请输入门牌号"></u--input>
-					</u-form-item>
-					<u-form-item label="联系人" prop="contact">
-						<u-input v-model="uForm.contact" border="bottom" placeholder="联系人姓名">
-							<template slot="suffix">
-								<view class="orderMap tongxun">
-									通讯录<u-icon name="arrow-right" size="10px" color="" style="margin-left:6upx;">
-									</u-icon>
-								</view>
-							</template>
-						</u-input>
-					</u-form-item>
-					<u-form-item label="手机号" prop="phone">
-						<u--input v-model="uForm.phone" border="bottom" placeholder="请输入手机号"></u--input>
-					</u-form-item>
-					<u-form-item label="标签" prop="tag">
-						<u--input v-model="uForm.tag" border="bottom" placeholder="可自定义标签,最多7个字符"></u--input>
-					</u-form-item>
-					<view class="tags">
-						<view class="tag" v-for="(item,index) in tags" :key="index" @click="xuanzhong(item)"
-							:class="{'xuanzhong':xuanzhongTag(item)}">
-							{{item}}
+			<!-- 搜索提示选项 -->
+			<view class="searchHint" v-show="showSearchHint">
+				<view class="search_item" v-for="item in 7">
+					<image class="locateIcon" src="@/static/carType/position.png" mode="widthFix"></image>
+					<view class="searchInfo">
+						<view class="place">
+							<view class="name">
+								服装批发大卖场
+							</view>
+							<view class="kilometre">
+								4.0km
+							</view>
+						</view>
+						<view class="detailAddress">
+							重庆市渝中区昆仑大道51号重庆火车站北站北广场上单123123
+						</view>
+						<view class="tags">
+							<view class="tag">
+								一号门
+							</view>
+							<view class="tag">
+								地下停车场
+							</view>
 						</view>
 					</view>
-				</u--form>
+				</view>
+
+
 			</view>
 
-		</view>
-		<view class="bt">
-			<u-button color="#02A7F0" text="保存" @click="onsubmit()"></u-button>
 		</view>
 	</view>
 </template>
 
 <script>
 	import Header from '@/components/header/header.vue'
-	var Contacts
 	export default {
 		data() {
 			return {
-				uForm: {
-					id: '',
-					tags: [],
-				},
-				list: [],
-				rules: {
-					phone: [{
-						required: false,
-						message: '请输入正确的格式',
-						pattern: this.$regExp.numberPhone3,
-						trigger: ['change', 'blur']
-					}],
-				},
-				tags: ['物流', '仓库', '店铺'],
-			};
-		},
-		methods: {
-			//标签选中
-			xuanzhong(item) {
-				let index = this.uForm.tags.indexOf(item)
-				if (index != -1) {
-					this.uForm.tags.splice(index, 1)
-				} else {
-					this.uForm.tags.push(item)
-				}
-			},
-			// 处理标签颜色
-			xuanzhongTag(val) {
-				let k = false
-				this.uForm.tags.forEach(value => {
-					if (value == val) {
-						k = true
-					}
-				})
-				return k
-			},
-			//获取地址详情
-			async get_addr_detail() {
-				const {
-					data: res
-				} = await uni.$http.api.address.get_addr_detail({
-					id: this.uForm.id
-				})
-				if (res.code === 200) {
-					res.data.tags = res.data.tags.split(',')
-					this.uForm = res.data
-				}
-			},
-			// 右侧删除按钮
-			async rightClick() {
-				//删除地址
-				await uni.$http.api.address.del_addr_detail({
-					id: this.uForm.id
-				})
-				uni.navigateBack()
-			},
-			//保存
-			onsubmit() {
-				this.$refs.uForm.validate().then(res => {
-					if (res) {
-						if (this.uForm.name) {
-							const da = JSON.parse(JSON.stringify(this.uForm))
-							da.tags = da.tags + ''
-							this.save_addr_book(da)
+				id: 0, // 使用 marker点击事件 需要填写id
+				title: 'map',
+				scale: 13, //地图缩放级别
+				latitude: 39.909,
+				longitude: 116.39742,
+				marker: [{
+					latitude: 39.909,
+					longitude: 116.39742,
 
-						} else {
-							uni.$showMsg('请选择地址')
-						}
-					}
-				})
-			},
-			//添加编辑地址
-			async save_addr_book(da) {
-				const {
-					data: res
-				} = await uni.$http.api.address.save_addr_book(da)
-				console.log(res);
-				uni.navigateTo({
+					iconPath: '../../../static/carType/shipment.png'
+				}],
+				shippingValue: '', //发货地址
+				deliveryValue: '', //交货地址
+				windowHeight: '',
+				showSearchHint: false,
+			}
+		},
+
+		methods: {
+			//添加地址按钮
+			submitBtn() {
+				uni.redirectTo({
 					url: '/subPackages/order/addressBook/addressBook'
 				})
 			},
-			//更改地址
-			changeOfAddress() {
-				uni.navigateTo({
-					url: '/subPackages/order/searchMap/searchMap'
-				})
+
+			// 寄 输入框改变时 
+			changeShipInput(event) {
+				if (this.shippingValue || this.deliveryValue) {
+					this.showSearchHint = true
+				} else {
+					this.showSearchHint = false
+				}
+
+				this.shippingValue = event.detail.value
+				console.log(this.shippingValue);
 			},
-		},
-		onLoad(option) {
-			if (option.id) {
-				this.uForm.id = option.id
-				this.get_addr_detail()
+			changedeliveryInput(event) {
+				if (this.shippingValue || this.deliveryValue) {
+					this.showSearchHint = true
+				} else {
+					this.showSearchHint = false
+				}
+				this.deliveryValue = event.detail.value
+				console.log(this.deliveryValue);
+			},
+			clearShippingValue() {
+				this.shippingValue = ''
+			},
+			clearDeliveryValue() {
+				this.DeliveryValue = ''
 			}
+
+
+		},
+		onLoad() {
+
 		},
 		components: {
 			Header
-		},
+		}
 	}
 </script>
 
 <style lang="scss" scoped>
-	.addAddress {
-		height: 100vh;
-		display: flex;
-		flex-direction: column;
+	.box {
+
+		width: 750rpx;
+		position: relative;
 
 
-		.addressBox {
+		.map {
+			width: 750rpx;
+			height: calc(100vh - 88rpx);
 			box-sizing: border-box;
-			padding: 20upx;
 
-			.tishi {
-				margin-bottom: 20upx;
-				margin-left: 20upx;
-				font-size: 24upx;
-				color: #949494;
+			//去掉高德地图 下方logo及提示文本
+			/deep/ .amap-logo {
+				display: none !important;
 			}
 
-			.address {
-				display: flex;
-				align-items: center;
-				box-sizing: border-box;
-				padding: 30upx 30upx;
-				background-color: #fff;
-				border-radius: 16upx;
-
-				.left {
-					flex: 1;
-					display: flex;
-					flex-direction: column;
-
-					.name {
-						color: #000000;
-						font-size: 28upx;
-						flex: 1;
-						font-weight: 600;
-					}
-
-					.xiangxi {
-						flex: 1;
-						margin-top: 16upx;
-						color: #949494;
-						font-size: 24upx;
-						overflow: hidden;
-						text-overflow: ellipsis;
-						-webkit-line-clamp: 1;
-						display: -webkit-box;
-						-webkit-box-orient: vertical;
-					}
-				}
-
-				.right {
-					margin-left: 20upx;
-					border-radius: 8upx;
-					box-sizing: border-box;
-					padding: 10upx 20upx;
-					color: #00A0E9;
-					border: 2upx solid #00A0E9;
-					font-size: 24upx;
-				}
-			}
-
-			.jianjie {
-				margin-top: 20upx;
-				box-sizing: border-box;
-				padding: 20upx 30upx;
-				background-color: #fff;
-				border-radius: 16upx;
-
-				.tongxun {
-					display: flex;
-					color: #02A7F0;
-					font-size: 24upx;
-				}
-
-				.tags {
-					box-sizing: border-box;
-					padding-left: 120upx;
-					display: flex;
-					flex-wrap: wrap;
-
-					.tag {
-						margin-bottom: 20upx;
-						border-radius: 8upx;
-						border: 2upx solid #dadada;
-						font-size: 24upx;
-						padding: 6upx 16upx;
-						margin-right: 20upx;
-					}
-
-					.xuanzhong {
-						color: #00A0E9;
-						border: 2upx solid #00A0E9;
-					}
-				}
+			/deep/ .amap-copyright {
+				opacity: 0;
 			}
 		}
 
-		.bt {
-			position: fixed;
-			bottom: 0upx;
+		.routeAddressBox {
+			position: absolute;
+			top: 29rpx;
+			left: 30rpx;
+			width: 690rpx;
+			height: 300rpx;
+			background-color: #fff;
+			border-radius: 20rpx;
+			padding: 38rpx 49rpx 36rpx 48rpx;
 			box-sizing: border-box;
-			padding: 0upx 60upx 60upx;
-			width: 100%;
+
+			// 分割线
+			.divider {
+				width: 502rpx;
+				height: 4rpx;
+				opacity: 0.5;
+				background-color: #EEF1F5;
+				margin-left: auto;
+			}
+
+
+			.AddressInput {
+				display: flex;
+
+				align-items: center;
+
+				>.icon {
+					width: 56rpx;
+					height: 56rpx;
+					background-color: #02A7F0;
+					border-radius: 50%;
+					color: #fff;
+					line-height: 56rpx;
+					text-align: center;
+					font-size: 28rpx;
+					margin-right: 36rpx;
+				}
+
+				.input_wrapper {
+					display: flex;
+					align-items: center;
+
+					.clearBtn {
+						height: 28rpx;
+						width: 28rpx;
+					}
+
+					>input {
+						height: 80rpx;
+						width: 473rpx;
+					}
+				}
+			}
+
+			.submitBtn {
+				background-color: #02A7F0;
+				height: 90rpx;
+				width: 600rpx;
+				border-radius: 10rpx;
+				line-height: 90rpx;
+				text-align: center;
+				color: #fff;
+				font-size: 30rpx;
+				font-weight: bold;
+				margin-top: 36rpx;
+			}
+		}
+
+		.messagePrompt {
+			position: absolute;
+			left: 30rpx;
+			top: 401rpx;
+			display: flex;
+			align-items: center;
+			background-color: #D8F3FF;
+			width: 690rpx;
+			height: 56rpx;
+			border-radius: 10rpx;
+			padding: 0 20rpx;
+			box-sizing: border-box;
+
+			.iconPic {
+				height: 24rpx;
+				width: 24rpx;
+				margin-right: 20rpx;
+			}
+
+			>.text {
+				font-size: 24rpx;
+				color: #00A0E9;
+			}
+
+		}
+
+		.searchHint {
+			position: absolute;
+			left: 30rpx;
+			top: 487rpx;
+			height: 53vh;
+			width: 690rpx;
+			background-color: #fff;
+			border-radius: 20rpx;
+			box-sizing: border-box;
+			overflow: scroll;
+
+
+
+			.search_item {
+				border-bottom: 1.5rpx solid #EBEFF4;
+
+				width: 100%;
+				display: flex;
+				box-sizing: border-box;
+				padding: 27rpx 26rpx 16rpx 44rpx;
+
+				border-bottom: 1.5rpx solid #EBEFF4;
+
+				&:last-child {
+					border-bottom: none;
+				}
+
+				>.locateIcon {
+					width: 24rpx;
+					height: 24rpx;
+					margin-right: 20rpx;
+					margin-top: 10rpx;
+
+				}
+
+				.searchInfo {
+					width: 576rpx;
+
+					>.place {
+						display: flex;
+						justify-content: space-between;
+						margin-bottom: 10rpx;
+
+						>.name {
+							color: #000000;
+							font-size: 28rpx;
+						}
+
+						>.kilometre {
+							color: #949494;
+							font-size: 22rpx;
+						}
+					}
+
+					>.detailAddress {
+						width: 576rpx;
+						color: #949494;
+						font-size: 24rpx;
+						white-space: nowrap;
+						text-overflow: ellipsis;
+						overflow: hidden;
+					}
+
+					.tags {
+						margin-top: 16rpx;
+						display: flex;
+						flex-wrap: wrap;
+
+						.tag {
+							height: 48rpx;
+							border-radius: 10rpx;
+							border: 1rpx solid rgba(0, 0, 0, 0.20);
+							font-size: 24rpx;
+							box-sizing: border-box;
+							margin-right: 30rpx;
+
+							padding: 8rpx 10rpx 7rpx;
+							text-align: center;
+							color: rgba(0, 0, 0, 1);
+						}
+					}
+				}
+
+			}
+
 		}
 	}
 </style>
